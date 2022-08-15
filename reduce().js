a108cf88bafa05e2dda70334701cec97d62f70ac
DucAnh1312 <109054934+DucAnh1312@users.noreply.github.com>
2022-08-15 09:29:15 +0700

    Delete reduce().js

--- reduce().js
+++
@@ -1,9 +0,0 @@
-// myReduce method
-Array.prototype.myReduce = function (callback) {
-  var a = 0;
-  for (let i = 0; i < this.length; i++) {
-    callback((a = a + this[i]));
-  }
-
-  return a;
-};
